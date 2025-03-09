cb58dc9e70af58bae27f93c3cad7517092054613
ahmadhian.daffa <ahmadhian.daffa@gmail.com>
2025-03-09 16:16:45 +0700

    Add main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+import shutil
+import os
+import csv
+import patoolib
+
+filename = "submission.zip"
+extract_dir = "Extract"
+grader_file = "grader_list.csv"
+
+# # Unzip submission file
+# shutil.unpack_archive(filename, extract_dir)
+
+# # Remove unnecessary folder name part
+# folders = os.listdir(extract_dir)
+# for folder in folders:
+#     old_name = folder
+#     old_path = os.path.join(extract_dir, old_name)
+#     new_name = folder.split("_")[0]
+#     new_path = os.path.join(extract_dir, new_name)
+#     os.rename(old_path, new_path)
+
+# # Read grader file
+# with open(grader_file) as fp:
+#     reader = csv.reader(fp)
+#     next(reader, None)  # skip the headers
+#     student_list = [row for row in reader]
+
+# # Add NRP & grader to folder name
+# for student in student_list:
+#     nrp, name, grader = student
+
+#     old_name = name
+#     old_path = os.path.join(extract_dir, old_name)
+#     new_name = grader + "_" + nrp[-3:] + "_" + name 
+#     new_path = os.path.join(extract_dir, new_name)
+#     os.rename(old_path, new_path)
+
+# Extract file in each folder
+folders = os.listdir(extract_dir)
+for folder in folders:
+    folder_path = os.path.join(extract_dir, folder)
+    files = os.listdir(folder_path)
+    for file in files:
+        file_path = os.path.join(folder_path, file)
+        _, extension = os.path.splitext(file)
+        if extension == ".zip":
+            print(file)
+            shutil.unpack_archive(file_path, format="zip", extract_dir=folder_path)
+            os.remove(file_path) 
+        elif extension == ".rar":
+            shutil.unpack_archive(file_path, format="rar")
+            patoolib.extract_archive(file_path)
+        else:
+            continue
