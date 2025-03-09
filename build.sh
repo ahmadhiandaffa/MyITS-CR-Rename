e902707f1e17e611ed2d6e7cd00ba51d8a842d83
ahmadhian.daffa <ahmadhian.daffa@gmail.com>
2025-03-09 21:20:33 +0700

    Fix typo & add one liner

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 pip install pyinstaller
-pyinstaller \
-    --onefile \
-    --windowed \
-    --icon="icon.ico" \
-    --distpath="./" \
-    --clean \
-    --specpath="./" \
-    --name="MyITS CR Rename" \
-    --upx-dir="./ \
-    main.py
+# pyinstaller \
+#     --onefile \
+#     --windowed \
+#     --icon="icon.ico" \
+#     --distpath="./" \
+#     --clean \
+#     --specpath="./" \
+#     --name="MyITS CR Rename" \
+#     --upx-dir="./" \
+#     main.py
+
+pyinstaller --onefile --windowed --icon="icon.ico" --distpath="./" --clean --specpath="./" --name="MyITS CR Rename" --upx-dir="./" main.py
